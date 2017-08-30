22b554f4d65266a790589f03506d76f97ecb7d85
Viacheslav Luschinskiy <slaval@mail.ru>
2017-08-30 18:30:17 +0300

    Домашнее задание

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -16,6 +16,8 @@ export const SIGN_UP_REQUEST = `${appName}/${moduleName}/SIGN_UP_REQUEST`
 export const SIGN_UP_SUCCESS = `${appName}/${moduleName}/SIGN_UP_SUCCESS`
 export const SIGN_UP_ERROR = `${appName}/${moduleName}/SIGN_UP_ERROR`
 export const SIGN_IN_SUCCESS = `${appName}/${moduleName}/SIGN_IN_SUCCESS`
+export const SIGN_IN_REQUEST = `${appName}/${moduleName}/SIGN_IN_REQUEST`
+export const SIGN_IN_ERROR = `${appName}/${moduleName}/SIGN_IN_ERROR`
 export const SIGN_OUT_REQUEST = `${appName}/${moduleName}/SIGN_OUT_REQUEST`
 export const SIGN_OUT_SUCCESS = `${appName}/${moduleName}/SIGN_OUT_SUCCESS`
 
@@ -25,6 +27,7 @@ export default function reducer(state = new ReducerRecord(), action) {
 
     switch (type) {
         case SIGN_UP_REQUEST:
+        case SIGN_IN_REQUEST:
             return state.set('loading', true)
 
         case SIGN_IN_SUCCESS:
@@ -34,6 +37,7 @@ export default function reducer(state = new ReducerRecord(), action) {
                 .set('error', null)
 
         case SIGN_UP_ERROR:
+        case SIGN_IN_ERROR:
             return state
                 .set('loading', false)
                 .set('error', error)
@@ -50,6 +54,38 @@ export function signUp(email, password) {
     return {
         type: SIGN_UP_REQUEST,
         payload: {email, password}
+    }
+}
+
+export function signIn(email, password) {
+    return {
+        type: SIGN_IN_REQUEST,
+        payload: {email, password}
+    }
+}
+
+export const watchSignIn = function*() {
+   yield takeEvery(SIGN_IN_REQUEST, signInSaga)
+}
+
+export const signInSaga = function* (action) {
+    const auth = firebase.auth()
+    console.log('I have started')
+    
+    try {
+        const user = yield call(
+            [auth, auth.signInWithEmailAndPassword],
+            action.payload.email, action.payload.password
+        )
+        yield put({
+            type: SIGN_IN_SUCCESS,
+            payload: {user}
+        })
+    } catch (error) {
+        yield put({
+            type: SIGN_IN_ERROR,
+            error
+        })
     }
 }
 
@@ -83,29 +119,10 @@ export const signUpSaga = function * () {
     }
 }
 
-/*
-export function signUp(email, password) {
-    return (dispatch) => {
-        dispatch({
-            type: SIGN_UP_REQUEST
-        })
-
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(user => dispatch({
-                type: SIGN_UP_SUCCESS,
-                payload: {user}
-            }))
-            .catch(error => dispatch({
-                type: SIGN_UP_ERROR,
-                error
-            }))
-    }
-}
-*/
 
 export const watchStatusChange = function * () {
     const auth = firebase.auth()
-    try {
+     try {
         yield cps([auth, auth.onAuthStateChanged])
     } catch (user) {
         yield put({
@@ -115,16 +132,7 @@ export const watchStatusChange = function * () {
     }
 }
 
-/*
-firebase.auth().onAuthStateChanged(user => {
-    const store = require('../redux').default
-    store.dispatch({
-        type: SIGN_IN_SUCCESS,
-        payload: {user}
-    })
-})
 
-*/
 
 export const signOutSaga = function * () {
     const auth = firebase.auth()
@@ -144,6 +152,7 @@ export const saga = function * () {
     yield all([
         signUpSaga(),
         watchStatusChange(),
+        watchSignIn(),
         takeEvery(SIGN_OUT_REQUEST, signOutSaga)
     ])
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,15 @@
 import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
-import logger from 'redux-logger'
+//import logger from 'redux-logger'
 import {routerMiddleware} from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
 import history from '../history'
 import rootSaga from './saga'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const sagaMiddleware = createSagaMiddleware()
-const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger)
+const enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware, routerMiddleware(history)))
 
 const store = createStore(reducer, enhancer)
 window.store = store
