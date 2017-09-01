44cfa5126ba8c320ca4fe9c30f2bf7f6f7dc2dd3
Alexey Vakarchuk <alexeyvakarchuk@yandex.ru>
2017-09-01 15:56:02 +0300

    Add HT2.1

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore,  applyMiddleware, compose} from 'redux'
 import reducer from './reducer'
 import logger from 'redux-logger'
 import {routerMiddleware} from 'react-router-redux'
@@ -9,7 +9,11 @@ import rootSaga from './saga'
 const sagaMiddleware = createSagaMiddleware()
 const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger)
 
-const store = createStore(reducer, enhancer)
+const store = createStore(reducer, compose(
+        enhancer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
 window.store = store
 
 sagaMiddleware.run(rootSaga)
