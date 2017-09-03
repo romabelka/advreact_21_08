ebbe6a6e77139e5c562a84daeca42d5865692c7e
kaflan <kaf_90@mail.ru>
2017-09-03 18:34:14 +0300

    fix erorr

--- src/ducks/auth.test.js
+++ src/ducks/auth.test.js
@@ -13,30 +13,36 @@ import {
 } from './auth'
 import firebase from 'firebase'
 import {call, put, take} from 'redux-saga/effects'
+const AUTH = firebase.auth()
+const person = {
+    email: 'kaf_90@mail.ru',
+    password: '11111111'
+}
 
 it('auth by sign up', () => {
-    const person = {
-        email: 'kaf_90@mail.ru',
-        password: '11111111'
-    }
+    
     const saga = signUpSaga()
-
-    expect(saga.next({type: SIGN_UP_SUCCESS, payload: person})).toEqual(take(SIGN_UP_REQUEST));
-    // expect(saga.next({type: SIGN_UP_SUCCESS, payload: person})).toEqual(put({type: SIGN_UP_SUCCESS, payload: person}));
-
+    expect(saga.next().value).toEqual(take(SIGN_UP_REQUEST))
+    expect(saga.next({type: SIGN_UP_REQUEST, payload: person}).value).toEqual(call([AUTH, AUTH.createUserWithEmailAndPassword], person.email, person.password));
+    expect(saga.next(person).value).toEqual(put({
+                type:SIGN_UP_SUCCESS,
+                payload:{user:person}
+        }))
+    // expect(saga.throw()).toEqual({
+    //     type: SIGN_UP_ERROR
+    // })
 })
 
 
 it('auth by sign in', () => {
-    const person = {
-        email: 'kaf_90@mail.ru',
-        password: '11111111'
-    }
     const saga = signInSaga()
     expect(saga.next().value).toEqual(take(SIGN_IN_REQUEST))
-    expect(saga.next({type: SIGN_IN_SUCCESS, payload: person}))
-        .toEqual(put({type: SIGN_IN_SUCCESS, payload: person}))
-
-//     // debugger;
-
+    expect(saga.next({type: SIGN_IN_REQUEST, payload: person}).value).toEqual(call([AUTH, AUTH.signInWithEmailAndPassword], person.email, person.password));
+    expect(saga.next(person).value).toEqual(put({
+        type:SIGN_IN_SUCCESS,
+        payload:{user:person}
+}))
+    // expect(saga.throw()).toEqual({
+    //     type: SIGN_UP_ERROR
+    // })
 })
