8f307c08a04b84f35772d8a8be54a9943184a1a7
Kirill <908city@gmail.com>
2017-09-16 14:00:00 +0400

    Add socket

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
-export const appName = "advreact-21-08"
+export const appName = "advreact-21-081"
 export const firebaseConfig = {
-    apiKey: "AIzaSyDjA6CeIHuni5lNm4ML1b-TSxJltsYUO8g",
-    authDomain: `${appName}.firebaseapp.com`,
-    databaseURL: `https://${appName}.firebaseio.com`,
-    projectId: appName,
-    storageBucket: `${appName}.appspot.com`,
-    messagingSenderId: "789814589283"
+    apiKey: "AIzaSyC0hcpDsBhr85usoHIwloBeYbkDHeUcqKo",
+    authDomain: "advreact-21-081.firebaseapp.com",
+    databaseURL: "https://advreact-21-081.firebaseio.com",
+    projectId: "advreact-21-081",
+    storageBucket: "advreact-21-081.appspot.com",
+    messagingSenderId: "450237050585"
 }
 
 firebase.initializeApp(firebaseConfig)
